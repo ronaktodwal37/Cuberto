321f7395895447a581f91701b97d9072d0b1f5b7
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-22 23:38:46 +0530

    Done changes

---
+++ script.js
@@ -0,0 +1,56 @@
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+});
+
+gsap.to(".fleftelm", {
+  scrollTrigger: {
+    trigger: "#fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftelm");
+// Shery.imageEffect(".images", {
+//   style: 4,
+//   config: { onMouse: { value: 1 } },
+//   slideStyle: (setScroll) => {
+//     sections.forEach(function (section, index) {
+//       ScrollTrigger.create({
+//         trigger: section,
+//         start: "top top",
+//         scrub: 1,
+//         onUpdate: function (prog) {
+//           setScroll(prog.progress + index);
+//         },
+//       });
+//     });
+//   },
+// });
+
+Shery.imageEffect(".images", {
+    style: 4
+});
+
+
+// Shery.imageEffect(".images", {
+//   style: 4,
+//   config: {
+//     onMouse: {
+//       value: 1
+//     }
+//   }
+// });
